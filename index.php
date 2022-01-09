0266180950f658593fed4d5dc8e0136375701736
Guilherme <gui.guilhermewc@gmail.com>
2022-01-09 18:54:25 -0300

    Primeiros arquivos do projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//Objetivo criar uma calculadora de imc e falar se a passoa está acima do peso, abaixo do peso ou no peso ideal.
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
